ce61eb624c54a98353636c56d1a5122dfc3a857f
drbarq <joe.tustin@datadoghq.com>
2022-11-08 09:27:05 -0700

    updating react

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
-const tracer = require('dd-trace').init()
+import tracer from "dd-trace";
 import calculate from "../logic/calculate";
 import "./App.css";
 
+tracer.init()
 
 export default class App extends React.Component {
   state = {
